f100f93bdcb8ae3a383ce362b82166184ea69992
Lindsay <lin.barrero.martinez@gmail.com>
2024-11-06 17:09:54 +0100

    Added customized spinner component

--- app/src/main/java/dam/pmdm/navigationdrawer/HomeFragment.kt
+++ app/src/main/java/dam/pmdm/navigationdrawer/HomeFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import dam.pmdm.navigationdrawer.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
 
@@ -18,6 +20,19 @@ class HomeFragment : Fragment() {
     ): View? {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false) // Inicializar binding
+
+        initialiceSpinner()
         return binding.root // Retornar la vista raíz
+    }
+
+    private fun initialiceSpinner() {
+        val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
+            requireContext(), R.array.animals, R.layout.spinner_item
+        )
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
+        binding.spinner.adapter = adapter
+
+        // Asignar el adaptador al Spinner
+        binding.spinner.adapter = adapter
     }
 }
